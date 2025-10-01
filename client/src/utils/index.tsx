9d73fc603311fb9b874219fcdf3f8cdbb455ed7b
Alistair Abao <abaolester@gmail.com>
2025-10-01 23:22:37 +0200

    created ErrorPage boundary using useRouteError

--- client/src/utils/index.tsx
+++ client/src/utils/index.tsx
@@ -1,3 +1,4 @@
 export { default as HomepageLayout } from "../pages/HomepageLayout";
 export { default as LandingPage } from "../pages/LandingPage";
 export { default as RegisterPage } from "../pages/RegisterPage";
+export { default as ErrorPage } from "../pages/ErrorPage";
