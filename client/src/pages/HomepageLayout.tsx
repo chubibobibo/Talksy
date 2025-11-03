00742e78797d1727bac19530021dd1e2846b766c
Alistair Abao <abaolester@gmail.com>
2025-11-03 23:55:45 +0100

    added about me in register

--- client/src/pages/HomepageLayout.tsx
+++ client/src/pages/HomepageLayout.tsx
@@ -3,19 +3,16 @@ import { Outlet } from "react-router-dom";
 function HomepageLayout() {
   return (
     <>
-      <section className='h-screen flex flex-col justify-between '>
+      <section className='h-screen flex flex-col justify-between'>
         <Outlet />
-        <section>
-          <footer className='footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4'>
-            <aside>
-              <p>
-                Copyright © {new Date().getFullYear()} - All right reserved by
-                ACME Industries Ltd
-              </p>
-            </aside>
-          </footer>
-        </section>
       </section>
+      {/* <section>
+        <footer className='footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-2 md:p-4'>
+          <aside>
+            <p>Copyright © {2025} - All right reserved by TALKSY</p>
+          </aside>
+        </footer>
+      </section> */}
     </>
   );
 }
